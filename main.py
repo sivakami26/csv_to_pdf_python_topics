9631f2e8eeec86109b486c2901140bc4cccdb6b8
sivakami <sivakami2607@gmail.com>
2023-10-11 10:38:05 +0530

    csv to pdf - adding correct number of pages

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ for index, row in df.iterrows():
     pdf.set_text_color(100,100,100)
     pdf.cell(w=0, h=12, txt=row["Topic"], align = "L", border=0, ln=0)
     pdf.line(10,21,200, 21)
-    for iterator in range(row["Pages"]):
+    for iterator in range(row["Pages"] - 1):
         pdf.add_page()
 pdf.output("output.pdf")
 
